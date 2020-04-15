f827b12f9e7c444ff09a8e8dbf919cded07f0503
LeonidK23 <kaida.leonid@gmail.com>
2020-04-15 20:59:39 +0200

    move all in one file

--- vis.py
+++ vis.py
@@ -1,28 +1,47 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import matplotlib.animation
+import pandas as pd
 
 import warnings
 import matplotlib.cbook
 
-from data_processing import *
-
 warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 
-flights = read_data("travels.csv")
-cities = get_cities(flights)
-city_coords = get_city_coords(cities)
-flights_coords = add_coords(flights, city_coords)
+# data processing
+with open("travels.csv") as f:
+    flights = f.readlines()
+
+flights = [l.strip().split(",") for l in flights]
+cities = set()
+for flight in flights:
+    source, dist = flight
+    cities.add(source)
+    cities.add(dist)
+
+city_coords = dict()
+df = pd.read_csv("worldcities.csv")
+related_data = df.loc[df['city_ascii'].isin(cities)]
+for city in cities:
+    city_coords[city] = (0, 0)
+    city_data = related_data.loc[related_data['city_ascii'] == city]
+    if not city_data.empty:
+        city_coords[city] = (city_data.iloc[0]['lat'], city_data.iloc[0]['lng'])
+
+for flight in flights:
+    dep = flight[0]
+    dest = flight[1]
+    flight += [city_coords[dep], city_coords[dest]]
 
 # draw map of europe
-fig = plt.figure(num=None, figsize=(15, 15))
+fig = plt.figure(num=None, figsize=(8, 8))
 m = Basemap(projection='merc', llcrnrlat=30, urcrnrlat=70, llcrnrlon=-13, urcrnrlon=42, resolution='l')
 m.drawcoastlines(linewidth=0.4)
 m.fillcontinents(color='darkgrey',lake_color='dimgrey')
 m.drawcountries()
 m.drawmapboundary(fill_color='dimgrey')
 
-for flight in flights_coords:
+for flight in flights:
     dep_lat, dep_lon = flight[2]
     dest_lat, dest_lon = flight[3]
     m.drawgreatcircle(dep_lon, dep_lat, dest_lon, dest_lat, color='yellow', linewidth=0.5)
@@ -39,6 +58,7 @@ for flight in flights_coords:
 #
 # ani = matplotlib.animation.FuncAnimation(fig, update, frames=len(x) + 1, interval=200, repeat=False)
 #
-# plt.tight_layout()
 # --------------------------------
-plt.show()
+plt.tight_layout()
+plt.savefig("static_2d.png", bbox_inches="tight")
+# plt.show()
